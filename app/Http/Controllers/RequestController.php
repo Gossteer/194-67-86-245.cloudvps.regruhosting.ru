89f7e2bf1772601cbe014fb540ac4849bfa3a3a0
Gossteer <GossteerPlus@gmail.com>
2021-04-14 21:42:33 +0300

    json_encode

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -13,16 +13,16 @@ class RequestController extends Controller
     {
         $userId = $request->post('user_id');
         $requests = RequestModel::where(['user_id' => $userId])->orderBy('id', 'DESC')->get();
-		for($i=0; $i<count($requests); $i++) {
-			$group_id = $requests[$i]->group_id;
-			$data = file_get_contents("https://api.vk.com/method/groups.getById?access_token=6070d8a06070d8a06070d8a03660036322660706070d8a03f3e1814c6491f7856970679&group_id=".$group_id."&v=5.124");
-			$tmp = json_decode($data, true);
+        for ($i = 0; $i < count($requests); $i++) {
+            $group_id = $requests[$i]->group_id;
+            $data = file_get_contents("https://api.vk.com/method/groups.getById?access_token=6070d8a06070d8a06070d8a03660036322660706070d8a03f3e1814c6491f7856970679&group_id=" . $group_id . "&v=5.124");
+            $tmp = json_decode($data, true);
             $requests[$i]->group_name = isset($group_id) ? $tmp['response'][0]['name'] : "";
-			$requests[$i]->group_photo = isset($group_id) ? $tmp['response'][0]['photo_200'] : "";
-			$requests[$i]->group_type = Group::getGroupType($group_id);
+            $requests[$i]->group_photo = isset($group_id) ? $tmp['response'][0]['photo_200'] : "";
+            $requests[$i]->group_type = Group::getGroupType($group_id);
             $requests[$i]['output'] = json_decode($requests[$i]['output'], true);
             $requests[$i]['content'] = json_decode($requests[$i]['content'], true);
-		}
+        }
 
         return response()->json([
             'requests' => $requests
@@ -39,8 +39,9 @@ class RequestController extends Controller
         ]);
     }
 
-	public function get_data($request_id) {
-		$request = RequestModel::find($request_id);
+    public function get_data($request_id)
+    {
+        $request = RequestModel::find($request_id);
 
         if ($request['content'] ?? false) {
             $request['content'] = json_decode($request['content']);
@@ -54,10 +55,10 @@ class RequestController extends Controller
             $request['output']->config->content = json_decode($request['output']->config->content);
         }
 
-		return response()->json([
+        return response()->json([
             'data' => $request
         ]);
-	}
+    }
 
     public function priceCalendar(Request $request)
     {
@@ -83,6 +84,33 @@ class RequestController extends Controller
 
     public function searchTickets(Request $request)
     {
-        return response()->json(['Piska']);
+        $response = Http::post('http://api.travelpayouts.com/v1/flight_search', [
+            'signature' =>  md5("d378bb3f3b879e6fc87899314ba5ce5d:back.aviabot.app:ru:122890:1:0:0:{$request['date_dst']}:{$request['dst']['code']}:{$request['src']['code']}:{$request['date_src']}:{$request['src']['code']}:{$request['dst']['code']}:Y:{$request->ip()}"),
+            "marker" => "122890",
+            "host" => "back.aviabot.app",
+            "user_ip" => $request->ip(),
+            "locale" => "ru",
+            "trip_class" => "Y",
+            "passengers" => [
+                "adults" => 1,
+                "children" => 0,
+                "infants" => 0
+            ],
+            "segments" => [
+                [
+                    "origin" => $request['src']['code'],
+                    "destination" =>  $request['dst']['code'],
+                    "date" => $request['date_dst']
+                ],
+                [
+                    "origin" => $request['dst']['code'],
+                    "destination" => $request['src']['code'],
+                    "date" => $request['date_src']
+                ]
+            ]
+        ])->json();
+        return response()->json([
+            'search_id' => $response['search_id']
+        ]);
     }
 }
