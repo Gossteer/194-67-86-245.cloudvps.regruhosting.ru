9fa2297e617a9713327e87a71ac8192c2ab89da0
Gossteer <GossteerPlus@gmail.com>
2021-06-06 19:46:42 +0300

    header

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -13,8 +13,8 @@ class IndexController extends Controller
     //Скорее всего больше не используется
     public function index(Request $request)
     {
-		//header("Access-Control-Allow-Origin: https://front.aviabot.app/");
-		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Origin: https://front.aviabot.app/");
+		// header("Access-Control-Allow-Origin: *");
 
         $user = User::find($request->vk_user_id) ?? new User();
 
--- app/Http/Controllers/UserDataController.php
+++ app/Http/Controllers/UserDataController.php
@@ -49,7 +49,7 @@ class UserDataController extends Controller
      */
     public function get(Request $request, $user_id): array
     {
-        header("Access-Control-Allow-Origin: https://front.aviabot.app/");
+        // header("Access-Control-Allow-Origin: https://front.aviabot.app/");
 
         $data = array();
 
