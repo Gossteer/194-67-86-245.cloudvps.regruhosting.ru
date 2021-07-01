bb7a249d7e3c7429c1e64002f29d1d61db81d0fd
Gossteer <GossteerPlus@gmail.com>
2021-07-01 22:29:39 +0300

    fix error search-id-tickets

--- app/Services/TravelPayoutsServices.php
+++ app/Services/TravelPayoutsServices.php
@@ -106,78 +106,79 @@ class TravelPayoutsServices
 
         try {
             $_SESSION['response_result'] = $response->wait()->getContents();
+
+
+            session_write_close();
+
+            $response_result = json_decode($_SESSION['response_result'], true);
+
+
+            // Проверяем уникальность найденных билетов по цене, даты отправке, и по IATA коду авиакомпании, выполняющей перевозку
+            $unique_value_check = [];
+            foreach ($response_result as $key => &$value) {
+                if ($value['proposals'] ?? false) {
+                    foreach ($value['proposals'] as $key_proposals => &$proposals) {
+                        $proposals['gates_info'] = &$value['gates_info'];
+                        if (($check_closure = function (&$unique_value_check, $proposals, $key, $key_proposals) {
+                            $price = $proposals['terms'][array_key_first($proposals['terms'])]['price'];
+                            $departure_date = $proposals['segment'][0]['flight'][0]['departure_timestamp'];
+                            $operating_carrier = $proposals['segment'][0]['flight'][0]['operating_carrier'];
+                            foreach ($unique_value_check as &$value) {
+                                foreach ($value as &$unique_value_check_proposals) {
+                                    if (
+                                        $unique_value_check_proposals['price'] == $price and
+                                        $unique_value_check_proposals['departure_timestamp'] == $departure_date and
+                                        $unique_value_check_proposals['operating_carrier'] == $operating_carrier
+                                    ) {
+                                        return true;
+                                    }
+                                }
+                            }
+                            $unique_value_check[$key][$key_proposals]['price'] = $price;
+                            $unique_value_check[$key][$key_proposals]['departure_timestamp'] = $departure_date;
+                            $unique_value_check[$key][$key_proposals]['operating_carrier'] = $operating_carrier;
+
+                            return false;
+                        })($unique_value_check, $proposals, $key, $key_proposals)) {
+                            unset($value['proposals'][$key_proposals]);
+                        }
+                    }
+                    $response_result[$key]['proposals'] =  array_values($response_result[$key]['proposals']);
+                } else {
+                    unset($response_result[$key]);
+                }
+            }
+
+            // Объединяем данные
+            unset($response_result[0]['filters_boundary'], $response_result[0]['meta'], $response_result[0]['chunk_id']);
+            foreach ($response_result as $key => &$value) {
+                $response_result[0]['proposals'] = array_merge($response_result[0]['proposals'], $value['proposals']);
+                $response_result[0]['gates_info'] = array_merge($response_result[0]['gates_info'], $value['gates_info']);
+                $response_result[0]['airports'] = array_merge($response_result[0]['airports'], $value['airports']);
+                $response_result[0]['airlines'] = array_merge($response_result[0]['airlines'], $value['airlines']);
+                $response_result[0]['flight_info'] = array_merge($response_result[0]['flight_info'], $value['flight_info']);
+                if ($key != 0) {
+                    unset($response_result[$key]);
+                }
+            }
+
+            // Сортируем по цене
+            usort($response_result[0]['proposals'], function ($value, $value_next) {
+                if (isset($value['terms'][array_key_first($value['terms'])]['price'], $value_next['terms'][array_key_first($value_next['terms'])]['price'])) {
+                    if ($value['terms'][array_key_first($value['terms'])]['price'] == $value_next['terms'][array_key_first($value_next['terms'])]['price']) {
+                        return 0;
+                    }
+                    return ($value['terms'][array_key_first($value['terms'])]['price'] < $value_next['terms'][array_key_first($value_next['terms'])]['price']) ? -1 : 1;
+                }
+            });
+
+            return [
+                array_values($response_result)[0]
+            ];
         } catch (\Throwable $th) {
             session_write_close();
             return [];
         }
-
-        session_write_close();
-
-        $response_result = json_decode($_SESSION['response_result'], true);
-
-
-        // Проверяем уникальность найденных билетов по цене, даты отправке, и по IATA коду авиакомпании, выполняющей перевозку
-        $unique_value_check = [];
-        foreach ($response_result as $key => &$value) {
-            if ($value['proposals'] ?? false) {
-                foreach ($value['proposals'] as $key_proposals => &$proposals) {
-                    $proposals['gates_info'] = &$value['gates_info'];
-                    if (($check_closure = function (&$unique_value_check, $proposals, $key, $key_proposals) {
-                        $price = $proposals['terms'][array_key_first($proposals['terms'])]['price'];
-                        $departure_date = $proposals['segment'][0]['flight'][0]['departure_timestamp'];
-                        $operating_carrier = $proposals['segment'][0]['flight'][0]['operating_carrier'];
-                        foreach ($unique_value_check as &$value) {
-                            foreach ($value as &$unique_value_check_proposals) {
-                                if (
-                                    $unique_value_check_proposals['price'] == $price and
-                                    $unique_value_check_proposals['departure_timestamp'] == $departure_date and
-                                    $unique_value_check_proposals['operating_carrier'] == $operating_carrier
-                                ) {
-                                    return true;
-                                }
-                            }
-                        }
-                        $unique_value_check[$key][$key_proposals]['price'] = $price;
-                        $unique_value_check[$key][$key_proposals]['departure_timestamp'] = $departure_date;
-                        $unique_value_check[$key][$key_proposals]['operating_carrier'] = $operating_carrier;
-
-                        return false;
-                    })($unique_value_check, $proposals, $key, $key_proposals)) {
-                        unset($value['proposals'][$key_proposals]);
-                    }
-                }
-                $response_result[$key]['proposals'] =  array_values($response_result[$key]['proposals']);
-            } else {
-                unset($response_result[$key]);
-            }
-        }
-
-        // Объединяем данные
-        unset($response_result[0]['filters_boundary'], $response_result[0]['meta'], $response_result[0]['chunk_id']);
-        foreach ($response_result as $key => &$value) {
-            $response_result[0]['proposals'] = array_merge($response_result[0]['proposals'], $value['proposals']);
-            $response_result[0]['gates_info'] = array_merge($response_result[0]['gates_info'], $value['gates_info']);
-            $response_result[0]['airports'] = array_merge($response_result[0]['airports'], $value['airports']);
-            $response_result[0]['airlines'] = array_merge($response_result[0]['airlines'], $value['airlines']);
-            $response_result[0]['flight_info'] = array_merge($response_result[0]['flight_info'], $value['flight_info']);
-            if ($key != 0) {
-                unset($response_result[$key]);
-            }
-        }
-
-        // Сортируем по цене
-        usort($response_result[0]['proposals'], function ($value, $value_next) {
-            if (isset($value['terms'][array_key_first($value['terms'])]['price'], $value_next['terms'][array_key_first($value_next['terms'])]['price'])) {
-                if ($value['terms'][array_key_first($value['terms'])]['price'] == $value_next['terms'][array_key_first($value_next['terms'])]['price']) {
-                    return 0;
-                }
-                return ($value['terms'][array_key_first($value['terms'])]['price'] < $value_next['terms'][array_key_first($value_next['terms'])]['price']) ? -1 : 1;
-            }
-        });
-
-        return [
-            array_values($response_result)[0]
-        ];
     }
 
     public function priceCalendar($origin, $destination, string $calendar_type = 'departure_date'): ?array
