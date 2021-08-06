4314234c069d438409c97cbf40360c80c43f37a7
Gossteer <GossteerPlus@gmail.com>
2021-08-06 10:55:35 +0300

    label link for sendSubscriptionSearchTickets

--- app/Services/FormationMessageServices.php
+++ app/Services/FormationMessageServices.php
@@ -124,7 +124,7 @@ class FormationMessageServices
             $response['keyboard'] = $this->makeRequestKeyboard(false, true, [
                 'open_link' => [
                     'link' => $url,
-                    'label' => 'Купить за 15 минут'
+                    'label' => 'Купить'
                 ]
             ]);
         } else {
